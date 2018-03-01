c5e5a7a8e667100c2ec77a49817d6ca054d3a549
unknown <santaguida1@outlook.com>
2018-03-01 03:23:55 -0500

    Finished REadStationFile

---
+++ ReadStationFile.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+"""
+from tkinter import filedialog
+from tkinter import *
+
+root = Tk()
+root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+"""
+def ReadStationFile(filename):
+    file = open(filename,"r");
+    n = 11;
+    count =0;
+    data=[None]*11;
+    
+    for line in file:
+        data[count]=line;
+        count = count +1;
+        
+    
+    StationSet = namedtuple('Station',['name','stnlat','stnlong','stnalt',\
+                                       'utc_offset','az_el_nlim','az_el_lim',\
+                                       'st_az_speed_max','st_el_speed_max']);
+    
+    az_el_limSet = namedtuple('az_el_limSet',['az','elmin','elmax']);
+    
+    
+    az_el_lim = az_el_limSet(data[6],data[7],data[8]);
+    
+    Station = StationSet(data[0],data[1],data[2],data[3],data[4],data[5],\
+                         az_el_lim,data[9],data[10],data[11]);
+                         
+    return Station;

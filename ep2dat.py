6def0f7b53b7a1cc23ea659e4e1ac84416e317d3
unknown <santaguida1@outlook.com>
2018-03-01 21:31:54 -0500

    fixed ep2dat function and created unittester

--- ep2dat.py
+++ ep2dat.py
@@ -1,4 +1,20 @@
+import datetime
 import time
 
-def ep2dat(JulianDate):
-    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(JulianDate))
+def ep2dat(NotJulianDate):
+    year = int(NotJulianDate[:2])
+    days = float(NotJulianDate[2:])
+    
+    if year<19:
+        year = 2000+year;
+    else:
+        year = 1900+year;
+    
+    #creates a date object starting at the given year
+    dt = datetime.datetime(year,1,1,0,0);
+    #computes a timd difference by the number of days in TLE
+    timediff = datetime.timedelta(days);
+    #adds the objects
+    final= dt + timediff;
+    # might be off by 1 day
+    return str(final)
---
+++ unittester.py
@@ -0,0 +1,23 @@
+import unittest
+from doy import doy
+from frcofd import frcofd
+from ep2dat import ep2dat
+
+class TestAROMethods(unittest.TestCase):
+    
+    def test_doy(self):
+        # Checks to see if it equals the expected values
+        self.assertEqual(doy(2018,3,1),60)
+        self.assertEqual(doy(2009,3,14),73)
+   
+    def test_dayFraction(self):
+        # Checks to see if the two answers are equal to within 7 decimal places
+        self.assertAlmostEqual(frcofd(3,23,14.444),0.141139398)
+        self.assertAlmostEqual(frcofd(12,3,14.12345678901234),0.502246799)
+
+    def test_ep2dat(self):
+        self.assertEqual(ep2dat('8650.28438588'),'1986-02-20 06:49:30.940032')
+        
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAROMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)

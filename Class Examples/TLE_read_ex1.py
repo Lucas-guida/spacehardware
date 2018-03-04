df8ac4f4d5ba442e4d23f847ef1a4b6c13498681
Your Name <mohdk995@my.yorku.ca>
2018-03-04 15:39:15 -0500

    Added a folder for class examples

---
+++ Class Examples/TLE_read_ex1.py
@@ -0,0 +1,33 @@
+import datetime as dt
+import numpy as nm
+import math
+
+tlefile = open(r'C:\Users\Mohammed\Desktop\YorkU\Year 5\ENG 4350 Space Hardware\Labs\P-1\gps-ops.txt','rt')
+
+line0 = tlefile.readline();
+line1 = tlefile.readline();
+line2 = tlefile.readline();
+
+tlefile.close()
+
+print(line0,line1,line2)
+# pull out Reference Epoch
+refEpoch_string= line1[18:32];
+refEpoch_float= nm.float(line1[18:32]);
+refEpochdt = dt.datetime.strptime(refEpoch_string[:5],'%y%j');
+hr = (refEpoch_float - math.floor(refEpoch_float))*24; 
+rem = hr - math.floor(hr);
+mins = rem*60;
+rem1 = mins - math.floor(mins);
+secs = 60*rem1;
+mcs = (rem1*60-math.floor(secs))*10**6;
+# refepochdt variable date time in UTC
+refEpochdt = refEpochdt.replace(hour = int(hr),minute = int(mins), second = int(secs),microsecond = int(mcs));
+# compute the delta to local time
+curr_time = dt.datetime.utcnow();
+curr_time = curr_time.replace(hour=17,minute=0,second=0,microsecond=0);
+timedelta = curr_time - refEpochdt;
+epsec = timedelta.total_seconds();
+
+
+

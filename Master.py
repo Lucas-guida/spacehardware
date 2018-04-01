66cbdb5d01ac353892bb9b6047639c7c1469d7d3
Lucas <santaguida1@outlook.com>
2018-04-01 11:01:24 -0400

    LOS correction

--- Master.py
+++ Master.py
@@ -25,7 +25,7 @@ duration = np.float(duration)*60; #conversion to seconds for OM Simulation time
 strt = dt.datetime.strptime(start_time[:19],'%Y/%m/%d %H:%M:%S');
 mills = int(float(start_time[19:])*1000*1000);
 dat = dt.timedelta(microseconds=mills);
-strt=strt;
+strt=strt + dat;
 
 # read station file with station params
 Station = ReadStationFile(Station_fp);
@@ -73,67 +73,75 @@ startTimesIndex = ['']*int(duration/stepSize);
 
 mod = ModelicaSystem('./Sattraj.mo', "Sattraj.Master","Modelica.Constants\n")
 print("  Sat No.          Name                         AOS                          LOS                  Min. Expected Level (dBm)");
-#for i in range(0,int(numSat),1):
+for i in range(0,int(numSat),1):
 #for i in range(0,0,1):
-i = 0  
-curr_sat = TLEData[i];
-curr_epoch = ep2dat(curr_sat.refepoch);
-delta_tstart[i] = (strt - curr_epoch).total_seconds();
+#i = 0  
+    curr_sat = TLEData[i];
+    curr_epoch = ep2dat(curr_sat.refepoch);
+    delta_tstart[i] = (strt - curr_epoch).total_seconds();
+        
+    #Start OM connection
+    # now we use this information to generate the LOS times
+    stepSize=60;
+    JDates = ep2JD(strt);
     
-#Start OM connection
-# now we use this information to generate the LOS times
-stepSize=60;
-JDates = ep2JD(strt);
-
-   
-mod.setParameters(raan2=TLEData[i].raan, inc2=TLEData[i].incl, argper2=TLEData[i].argper, az_vel_lim2 = Station.az_el_nlim, el_vel_lim2=Station.az_el_nlim, din2=JDates[0], tm2 =JDates[1])
-mod.setParameters(**{"GPS.M0":TLEData[i].meanan, "GPS.N0":TLEData[i].meanmo, "GPS.ecc":TLEData[i].eccn, "GPS.Ndot2":TLEData[i].ndot, "GPS.Nddot6":0, "GPS.tstart":delta_tstart[i]})
-mod.setParameters(**{'ARO.elevation':Station.stnalt, 'ARO.latitude':Station.stnlat, 'ARO.longitude': Station.stnlong})
-mod.setSimulationOptions(stopTime=duration, stepSize=stepSize)
-mod.setSimulationOptions(startTime=0.)
-mod.simulate()
-(times[i],Az,El,dAz,dEl)=mod.getSolutions("time","azimuth2","elevation2","dAz2","dEl2");
-Azarray[i] =Az; 
-Elarray[i] =El; 
-dAzarray[i] =dAz; 
-dElarray[i] =dEl;
-"""    
-start= False;
-startTime=0;
-satDuration=0;
-for j in range(0,len(times)-1):
-    if El.item(j) < float(Station.az_el_lim.elmax[0]) and El.item(j) > float(Station.az_el_lim.elmin[0]) and Az.item(j) != 10^60 and El.item(j) != 10^60:
-        if start == False:
-            start = True;
-            startTime= times[i][j];
-            startTimes[i]=startTime;
-            startTimesIndex[i]=j;
+       
+    mod.setParameters(raan2=TLEData[i].raan, inc2=TLEData[i].incl, argper2=TLEData[i].argper, az_vel_lim2 = Station.az_el_nlim, el_vel_lim2=Station.az_el_nlim, din2=JDates[0], tm2 =JDates[1])
+    mod.setParameters(**{"GPS.M0":TLEData[i].meanan, "GPS.N0":TLEData[i].meanmo, "GPS.ecc":TLEData[i].eccn, "GPS.Ndot2":TLEData[i].ndot, "GPS.Nddot6":0, "GPS.tstart":delta_tstart[i]})
+    mod.setParameters(**{'ARO.elevation':Station.stnalt, 'ARO.latitude':Station.stnlat, 'ARO.longitude': Station.stnlong})
+    mod.setSimulationOptions(stopTime=duration, stepSize=stepSize)
+    mod.setSimulationOptions(startTime=0.)
+    mod.simulate()
+    (times[i],Az,El,dAz,dEl)=mod.getSolutions("time","azimuth2","elevation2","dAz2","dEl2");
+    Azarray[i] =Az; 
+    Elarray[i] =El; 
+    dAzarray[i] =dAz; 
+    dElarray[i] =dEl;
+    
+    if i == 3:
+        Azarrayasd =Az; 
+        Elarrayasd =El; 
+        dAzarrayasd =dAz; 
+        dElarrayasd =dEl;
+        
+    start= False;
+    startTime=0;
+    satDuration=0;
+    #print(len(Azarray))
+    #print(float(Station.az_el_lim.elmax[0]))
+    for j in range(0,len(times[i])-1):
+        if El.item(j) < float(Station.az_el_lim.elmax[0]) and El.item(j) > float(Station.az_el_lim.elmin[0]) and Az.item(j) != 10^60 and El.item(j) != 10^60:
+            if start == False:
+                start = True;
+                startTime= times[i][j];
+                startTimes[i]=startTime;
+                startTimesIndex[i]=j;
+            else:
+                satDuration= satDuration + 60;
         else:
-            satDuration= satDuration + 60;
-    else:
-        if start != False:
-            break;
-startDelta = strt + dt.timedelta(seconds=startTime);
-endDelta = strt + dt.timedelta(seconds=startTime+satDuration);
+            if start != False:
+                break;
+    startDelta = strt + dt.timedelta(seconds=startTime);
+    endDelta = strt + dt.timedelta(seconds=startTime+satDuration);
+    
+    if satDuration==0
+        continue;
+    
+    print("   " + '{:2d}'.format(i) + "      " + TLEData[i].name + "   " + str(startDelta) + "   " + str(endDelta));
+    
 
-if satDuration==0:
-    continue;
-
-print("   " + '{:2d}'.format(i) + "      " + TLEData[i].name + "   " + str(startDelta) + "   " + str(endDelta));
-"""  
-print(i)
-time.sleep(3)
+    time.sleep(3)
 
 #times = initalize array of durations // # of seconds each sat is avaiable during interval
 a = int(input("\n Please select a satellite by its number \n"))
 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 print("             UTC                Az [deg]          El [deg]            Az-vel [deg/sec]           Elz-vel [deg/sec]         Range [km]         Range Rate [km/sec]   Doppler[kHz]  Level [dBm]")
 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-#UTC = strt + dt.timedelta(seconds=startTimes[a])
-UTC ="cashmeoutsidehowboutdat"
-'''azumith = Azarray[a][startTimesIndex[a]];
+UTC = strt + dt.timedelta(seconds=startTimes[a])
+
+azumith = Azarray[a][startTimesIndex[a]];
 elevation = Elarray[a][startTimesIndex[a]];
 dazumith = dAzarray[a][startTimesIndex[a]];
-delevation = dElarray[a][startTimesIndex[a]];'''
+delevation = dElarray[a][startTimesIndex[a]];
 #STKpoint('Pointing_Satellite_'+str(a),times[a],Azarray[a],Elarray[a]);
-#print( str(UTC) +"          "+ str(azumith) +"            "+ str(elevation) +"          "+ str(dazumith) +"         "+ str(delevation))  
+print( str(UTC) +"          "+ str(azumith) +"            "+ str(elevation) +"          "+ str(dazumith) +"         "+ str(delevation))  
--- theta_t.mo
+++ theta_t.mo
@@ -1,17 +1,15 @@
 function theta_t
   input Real din "JD of the time of interst (not time difference since J2000)";
   //input Real t "time of interest";
-  input Real tm "time of mindnight of day of interest";
+  input Real tm "JDtime of mindnight of day of interest";
+  input Real t "system time";
   output Real theta_t "GMST angle (deg)";
 protected
-  Real d = din - 2451545.0;
- // Real d = 1879.386107;
+  Real d = din+(t/86400) - 2451545.0;
+  Real dm = tm - 2451545.0;
   Real T = d/36525;
   Real theta_mid;
   Real r;
 algorithm
-  theta_mid := mod(24110.54841 + 8640184.812866*T +0.093104*T^2 - 0.0000062*T^3,86400)*360/86400;
-  r:=1.002737909350795 + 5.9006*10^(-11)*T -5.9*10^(-15)*T^2;
-  //theta_t := theta_mid + 360*r*(2453424.416366-2453423.500000)/86400;//(time of interest ... in this case start of tracking time - tmidnight of start of tracking period)
-  theta_t := mod(theta_mid + 360*r*((din-tm)*86400)/86400,360);//(time of interest ... in this case start of tracking time - tmidnight of start of tracking period)
-end theta_t; 
+ theta_t := mod(18.697374558 + 24.06570982441908*d,24)*360/24;
+end theta_t;
